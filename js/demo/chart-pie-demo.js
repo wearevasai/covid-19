93f0aa82e049f65dd1da4abd52cef2bd088f5258
wearevasai <63285039+wearevasai@users.noreply.github.com>
2020-10-22 18:44:00 +0530

    111 new cases today

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -9,7 +9,7 @@ var myPieChart = new Chart(ctx, {
   data: {
     labels: ["Unknown", "Males", "Females"],
     datasets: [{
-      data: [898, 14056, 9927],
+      data: [898, 14113, 9974],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
--- js/demo/recoverychart.js
+++ js/demo/recoverychart.js
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 45, 211, 1631, 6288, 6894, 5856, 4002],
+      data: [0, 45, 211, 1631, 6288, 6894, 5856, 4165],
     }],
   },
   options: {
