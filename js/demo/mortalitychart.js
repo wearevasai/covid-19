0050e5a740fd906316723e128d0d6a6ebf723d1d
wearevasai <63285039+wearevasai@users.noreply.github.com>
2020-12-01 18:58:39 +0530

    31 new cases today

--- js/demo/mortalitychart.js
+++ js/demo/mortalitychart.js
@@ -32,7 +32,7 @@ var ctx = document.getElementById("mortalityChart");
 var myLineChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ["Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov"],
+    labels: ["Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
     datasets: [{
       label: "Deaths",
       lineTension: 0.3,
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [0, 14, 17, 91, 129, 141, 82, 152, 48],
+      data: [0, 14, 17, 91, 129, 141, 82, 152, 48, 0],
     }],
   },
   options: {
