7b67b43d6b751bb3e0c29844ee0c6b9a0010599f
wearevasai <63285039+wearevasai@users.noreply.github.com>
2021-02-10 20:24:39 +0530

    19 new cases today

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -46,7 +46,7 @@ var myLineChart = new Chart(ctx, {
       pointHoverBorderColor: "rgba(78, 115, 223, 1)",
       pointHitRadius: 10,
       pointBorderWidth: 2,
-      data: [7, 127, 633, 3392, 6956, 4762, 6142, 4132, 1507, 1086, 582, 110],
+      data: [7, 127, 633, 3392, 6956, 4762, 6142, 4132, 1507, 1086, 582, 129],
     }],
   },
   options: {
