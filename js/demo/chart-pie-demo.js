b9236f1eaae13372e4bd1140c9401b3f41823123
wearevasai <63285039+wearevasai@users.noreply.github.com>
2021-05-15 19:31:03 +0530

    Gender Details

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -9,7 +9,7 @@ var myPieChart = new Chart(ctx, {
   data: {
     labels: ["Unknown", "Males", "Females"],
     datasets: [{
-      data: [898, 35518, 24973],
+      data: [898, 25660, 25095],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
