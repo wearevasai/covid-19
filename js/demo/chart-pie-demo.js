bf0f3cd3a6c6508798d0bb68533a26bb313b5744
wearevasai <63285039+wearevasai@users.noreply.github.com>
2021-05-19 08:56:32 +0530

    112 new cases yesterday

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -9,7 +9,7 @@ var myPieChart = new Chart(ctx, {
   data: {
     labels: ["Unknown", "Males", "Females"],
     datasets: [{
-      data: [898, 35803, 25230],
+      data: [898, 35977, 25368],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
