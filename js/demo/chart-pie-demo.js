46217b3f4a87befddb2ad286ae4f92dc3865acf6
wearevasai <63285039+wearevasai@users.noreply.github.com>
2021-06-24 21:22:25 +0530

    Rural Numbers

--- js/demo/chart-pie-demo.js
+++ js/demo/chart-pie-demo.js
@@ -9,7 +9,7 @@ var myPieChart = new Chart(ctx, {
   data: {
     labels: ["Unknown", "Males", "Females"],
     datasets: [{
-      data: [898, 38255, 27369],
+      data: [898, 383117, 27428],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
